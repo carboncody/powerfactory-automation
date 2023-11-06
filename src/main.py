e6c58e08b4327e052e2f70d4563f286e8f9a377d
Jarl Pallesen <jpa@naoma.dk>
2023-11-06 14:40:31 +0100

    CHORE: extracted functions to separate files: resolution and parse_csv

--- src/main.py
+++ src/main.py
@@ -1,37 +1,22 @@
 import pandas as pd
+from parse_csv import parse_csv
+from resolution import resolution
 
 def main():
-    print("Starting Parsing")
+    
 
     # Specify the file to read
     master_path = 'utils/example.csv'
 
-    # Read the CSV file
-    master_data = pd.read_csv(master_path)
+    # Parse the data
+    master_data = parse_csv(master_path)
 
-    # Convert 'tid' to a timedelta object assuming the format is HH.MM.SS
-    # We replace the periods with colons first
-    master_data['tid'] = pd.to_timedelta(master_data['tid'].str.replace('.', ':'))
-
-    # Sort the data by time, just in case it's not already
-    master_data = master_data.sort_values('tid')
-
-    # Calculate differences between each consecutive time entry
-    master_data['time_diff'] = master_data['tid'].diff()
-
-    # Find the largest gap
-    largest_gap = master_data['time_diff'].max()
-    
-     # Format the largest gap to remove the 'days' part, if the largest gap is less than 1 day
-    if largest_gap < pd.Timedelta(days=1):
-        largest_gap_str = str(largest_gap).split(' days ')[-1]
-    else:
-        # Here we keep the days part if the gap is actually larger than 1 day
-        largest_gap_str = str(largest_gap)
+    # Find the biggest gap in resolution
+    largest_gap_str = resolution(master_data)
 
     # Print the largest gap
     print(f"The biggest resolution is: {largest_gap_str} in HH:MM:SS")
-    print(f"The Resoluton in simulation is = {largest_gap_str} ")
+    print(f"The Resolution in simulation is = {largest_gap_str}")
 
 if __name__ == "__main__":
     main()
---
+++ src/parse_csv.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def parse_csv(file_path):
+    
+    print("Starting Parsing")
+    # Read the CSV file and replace periods with colons
+    master_data = pd.read_csv(file_path)
+    master_data['tid'] = pd.to_timedelta(master_data['tid'].str.replace('.', ':'))
+    # Sort the data by time
+    master_data = master_data.sort_values('tid')
+    print("Parsing done")
+    return master_data
+   
