7e52cad82b24d185d592e91c1a6dcb513c5c4691
Jarl Pallesen <jpa@naoma.dk>
2023-11-08 16:08:21 +0100

    Feat: initialise Powerfactory connectiom

--- src/pf_sim_run/init_PF.py
+++ src/pf_sim_run/init_PF.py
@@ -1,1 +1,14 @@
-import sys; 
+import sys;
+
+sys.path.append(r"C:\Program Files\DIgSILENT\PowerFactory 2017\python\3.6")
+import powerfactory as pf
+
+def init_PF():
+
+    try:
+        app = pf.GetApplicationExt()
+    # ... some calculations ...
+
+    except pf.ExitError as error:
+        print(error)
+        print('error.code = %d' % error.code)
